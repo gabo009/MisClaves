210d8384d27eebd331c168c4440d237daee7fea3
gabo009 <gabriel.herrera12@inacap.cl>
2018-07-12 00:24:58 -0400

    hola

--- app/src/main/java/com/example/nacho/misclaves/EditActivity.java
+++ app/src/main/java/com/example/nacho/misclaves/EditActivity.java
@@ -1,18 +1,22 @@
 package com.example.nacho.misclaves;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
     private EditText et_Nombre, et_Usuario, et_Url, et_Contrasena, et_Pass, et_Comentario;
     private TextView tv_Rut;
-    private Button btn_Guardar, btn_Eliminar;
+    private BottomNavigationView btm_menu;
     private Switch sw_Ver;
     int posicion;
     String rut;
@@ -28,17 +32,8 @@ public class EditActivity extends AppCompatActivity {
         et_Pass = (EditText) findViewById(R.id.et_Pass);
         et_Comentario = (EditText) findViewById(R.id.et_Comentario);
         tv_Rut = (TextView) findViewById(R.id.tv_Rut);
-        btn_Guardar = (Button) findViewById(R.id.btn_Guardar);
-        btn_Eliminar = (Button) findViewById(R.id.btn_Eliminar);
+        btm_menu = (BottomNavigationView) findViewById(R.id.btm_menu);
         sw_Ver = (Switch) findViewById(R.id.sw_Ver);
-
-        if (et_Nombre.getText().toString().isEmpty()||et_Usuario.getText().toString().isEmpty()||et_Contrasena.getText().toString().isEmpty()){
-            btn_Guardar.setEnabled(false);
-        }
-
-        et_Nombre.setOnFocusChangeListener(listener);
-        et_Usuario.setOnFocusChangeListener(listener);
-        et_Contrasena.setOnFocusChangeListener(listener);
 
         bundle = getIntent().getExtras();
         rut = bundle.getString("usuario");
@@ -52,42 +47,63 @@ public class EditActivity extends AppCompatActivity {
             et_Comentario.setText(bundle.getString("comentario"));
         }
 
-
-        btn_Guardar.setOnClickListener(new View.OnClickListener() {
+        btm_menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(EditActivity.this, ViewActivity.class);
-                if (bundle.containsKey("posicion")) {
-                    intent.putExtra("posicion", posicion);
-                    intent.putExtra("usuario", rut);
-                    intent.putExtra("nombre", et_Nombre.getText().toString());
-                    intent.putExtra("user", et_Usuario.getText().toString());
-                    intent.putExtra("url", et_Url.getText().toString());
-                    intent.putExtra("contrasena", et_Contrasena.getText().toString());
-                    intent.putExtra("comentario", et_Comentario.getText().toString());
-                    startActivity(intent);
-                    finish();
-                }else{
-                    intent.putExtra("usuario", rut);
-                    intent.putExtra("nombre", et_Nombre.getText().toString());
-                    intent.putExtra("user", et_Usuario.getText().toString());
-                    intent.putExtra("url", et_Url.getText().toString());
-                    intent.putExtra("contrasena", et_Contrasena.getText().toString());
-                    intent.putExtra("comentario", et_Comentario.getText().toString());
-                    startActivity(intent);
-                    finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.grabar:
+                        if (!et_Nombre.getText().toString().isEmpty()&&!et_Usuario.getText().toString().isEmpty()&&!et_Contrasena.getText().toString().isEmpty()){
+                            Intent intent = new Intent(EditActivity.this, ViewActivity.class);
+                            if (bundle.containsKey("posicion")) {
+                                intent.putExtra("posicion", posicion);
+                                intent.putExtra("usuario", rut);
+                                intent.putExtra("nombre", et_Nombre.getText().toString());
+                                intent.putExtra("user", et_Usuario.getText().toString());
+                                intent.putExtra("url", et_Url.getText().toString());
+                                intent.putExtra("contrasena", et_Contrasena.getText().toString());
+                                intent.putExtra("comentario", et_Comentario.getText().toString());
+                                startActivity(intent);
+                                finish();
+                                return true;
+                            } else {
+                                intent.putExtra("usuario", rut);
+                                intent.putExtra("nombre", et_Nombre.getText().toString());
+                                intent.putExtra("user", et_Usuario.getText().toString());
+                                intent.putExtra("url", et_Url.getText().toString());
+                                intent.putExtra("contrasena", et_Contrasena.getText().toString());
+                                intent.putExtra("comentario", et_Comentario.getText().toString());
+                                startActivity(intent);
+                                finish();
+                                return true;
+                            }
+                        }else {
+                            return false;
+                        }
+                    case R.id.eliminar:
+                        if (bundle.containsKey("posicion")) {
+                            Intent intent = new Intent(EditActivity.this, ViewActivity.class);
+                            intent.putExtra("usuario", rut);
+                            intent.putExtra("posicion", posicion);
+                            intent.putExtra("borrar", true);
+                            startActivity(intent);
+                            finish();
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    default:
+                        return false;
                 }
             }
         });
+
     }
-    View.OnFocusChangeListener listener = new View.OnFocusChangeListener() {
-        @Override
-        public void onFocusChange(View v, boolean hasFocus) {
-            if (et_Nombre.getText().toString().isEmpty() || et_Usuario.getText().toString().isEmpty() || et_Contrasena.getText().toString().isEmpty()) {
-                btn_Guardar.setEnabled(false);
-            } else {
-                btn_Guardar.setEnabled(true);
-            }
-        }
-    };
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(EditActivity.this, ViewActivity.class);
+        intent.putExtra("usuario", rut);
+        startActivity(intent);
+        finish();
+    }
 }
--- app/src/main/java/com/example/nacho/misclaves/ViewActivity.java
+++ app/src/main/java/com/example/nacho/misclaves/ViewActivity.java
@@ -37,23 +37,32 @@ public class ViewActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         rut = bundle.get("usuario").toString();
         nombrearchivo = "cuentas"+rut+".txt";
-        leerArchivo(nombrearchivo);
+        //grabarArchivo(nombrearchivo);
 
-        if (bundle.containsKey("nombre")){
-            cuentas.add(new Cuentas(bundle.get("nombre").toString(), bundle.get("user").toString(), bundle.get("url").toString(), bundle.get("contrasena").toString(), bundle.get("comentario").toString()));
-            grabarArchivo(nombrearchivo);
-        }
+        leerArchivo(nombrearchivo);
 
         adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cuentas);
         lv_Cuentas.setAdapter(adaptador);
 
+        if (bundle.containsKey("posicion")&&bundle.containsKey("borrar")) {
+            borrarUsuario(bundle.getInt("posicion"));
+        } else if (bundle.containsKey("posicion")){
+            Cuentas c = cuentas.get(bundle.getInt("posicion"));
+            c.setNombre(bundle.get("nombre").toString());
+            c.setUsuario(bundle.get("user").toString());
+            c.setUrl(bundle.get("url").toString());
+            c.setClave(bundle.get("contrasena").toString());
+            c.setObservacion(bundle.get("comentario").toString());
+            grabarArchivo(nombrearchivo);
+        } else if (bundle.containsKey("nombre")) {
+            cuentas.add(new Cuentas(bundle.get("nombre").toString(), bundle.get("user").toString(), bundle.get("url").toString(), bundle.get("contrasena").toString(), bundle.get("comentario").toString()));
+            grabarArchivo(nombrearchivo);
+        }
+
         lv_Cuentas.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                cuentas.remove(position);
-                grabarArchivo(nombrearchivo);
-                leerArchivo(nombrearchivo);
-                adaptador.notifyDataSetChanged();
+                borrarUsuario(position);
                 return true;
             }
         });
@@ -72,10 +81,8 @@ public class ViewActivity extends AppCompatActivity {
                 grabarArchivo(nombrearchivo);
                 startActivity(intent);
                 finish();
-
             }
         });
-
         ibt_Agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,10 +91,10 @@ public class ViewActivity extends AppCompatActivity {
                 grabarArchivo(nombrearchivo);
                 startActivity(intent);
                 finish();
-
             }
         });
     }
+
 
     public void leerArchivo(String archivo) {
         cuentas.clear();
@@ -95,7 +102,7 @@ public class ViewActivity extends AppCompatActivity {
             BufferedReader br = new BufferedReader(isr)) {
             String line = br.readLine();
             while (line != null) {
-                String[] datos = line.split("\\|");
+                String[] datos = line.split("\\|",-2);
                 cuentas.add(new Cuentas(datos[0], datos[1], datos[2], datos[3], datos[4]));
                 line = br.readLine();
             }
@@ -106,15 +113,20 @@ public class ViewActivity extends AppCompatActivity {
             Log.d("TAG_", "No se pudo leer archivo "+archivo);
         }
     }
-
     private void grabarArchivo(String archivo){
         try (OutputStreamWriter osw = new OutputStreamWriter(openFileOutput(archivo, Activity.MODE_PRIVATE))){
             for (Cuentas cuenta:cuentas) {
                 osw.write(cuenta.getNombre()+"|"+cuenta.getUsuario()+"|"+cuenta.getUrl()+"|"+cuenta.getClave()+"|"+cuenta.getObservacion()+"\n");
             }
+            //osw.write("");
         } catch (Exception ex){
             Log.d("TAG_", "No se pudo crear el archivo "+archivo);
         }
     }
-
+    private void borrarUsuario(int posicion){
+        cuentas.remove(posicion);
+        grabarArchivo(nombrearchivo);
+        leerArchivo(nombrearchivo);
+        adaptador.notifyDataSetChanged();
+    }
 }
